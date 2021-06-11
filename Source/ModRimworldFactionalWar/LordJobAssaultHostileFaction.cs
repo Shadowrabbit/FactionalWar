ce4f4303dd08444205e2a925bb56d16159dd48fb
BILIBILI\wangyue07 <wangyue07@bilibili.com>
2021-06-11 15:22:07 +0800

    设置命名空间

---
+++ Source/ModRimworldFactionalWar/LordJobAssaultHostileFaction.cs
@@ -0,0 +1,24 @@
+﻿// ******************************************************************
+//       /\ /|       @file       LordJobAssaultOtherFaction.cs
+//       \ V/        @brief      集群AI 突击敌对派系
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2021-06-11 09:44:29
+//    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
+// ******************************************************************
+using Verse.AI.Group;
+
+namespace SR.ModRimworld.FactionalWar
+{
+    public class LordJobAssaultHostileFaction : LordJob
+    {
+        /// <summary>
+        /// 创建状态机
+        /// </summary>
+        /// <returns></returns>
+        public override StateGraph CreateGraph()
+        {
+            return null;
+        }
+    }
+}
