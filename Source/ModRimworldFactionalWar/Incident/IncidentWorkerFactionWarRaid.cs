c930b693919be0cbdebbac8586c3243027381fa0
BILIBILI\wangyue07 <wangyue07@bilibili.com>
2021-06-22 19:41:06 +0800

    场地测试

---
+++ Source/ModRimworldFactionalWar/Incident/IncidentWorkerFactionWarRaid.cs
@@ -0,0 +1,32 @@
+﻿// ******************************************************************
+//       /\ /|       @file       IncidentWorkerFactionWarRaid.cs
+//       \ V/        @brief      派系战争袭击
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2021-06-22 12:05:02
+//    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
+// ******************************************************************
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace SR.ModRimWorld.FactionalWar
+{
+    [UsedImplicitly]
+    public class IncidentWorkerFactionWarRaid : IncidentWorkerFactionWarShelling
+    {
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            var temp = new List<SitePartDef>();
+            temp.Add(SitePartDefOf.SrCampInWar);
+            TileFinder.TryFindNewSiteTile(out var tileId, 4, 7);
+            var site = SiteMaker.TryMakeSite(temp, tileId, false, null, true, 10000);
+            Find.WorldObjects.Add(site);
+            parms.target = site.Map;
+            base.TryExecuteWorker(parms);
+            return true;
+        }
+    }
+}
---
+++ Source/ModRimworldFactionalWar/Site/SitePartDefOf.cs
@@ -0,0 +1,20 @@
+﻿// ******************************************************************
+//       /\ /|       @file       SitePartDefOf.cs
+//       \ V/        @brief      场地部分构成定义
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2021-06-22 12:24:07
+//    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
+// ******************************************************************
+
+using JetBrains.Annotations;
+using RimWorld;
+
+namespace SR.ModRimWorld.FactionalWar
+{
+    [DefOf]
+    public static class SitePartDefOf
+    {
+        [UsedImplicitly] public static readonly SitePartDef SrCampInWar; //处于战斗中的营地
+    }
+}
