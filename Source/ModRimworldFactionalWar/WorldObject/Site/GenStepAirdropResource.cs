3c924cc9245e85ad37e99f48928c4dede46dbda8
yingtu0401@gmail.com <yingtu0401@gmail.com>
2021-06-24 01:34:15 +0800

    生成空投物资；生成机械族威胁

--- Source/ModRimworldFactionalWar/WorldObject/Site/GenStepAirdropResource.cs
+++ Source/ModRimworldFactionalWar/WorldObject/Site/GenStepAirdropResource.cs
@@ -6,13 +6,19 @@
 //      /  \\        @Modified   2021-06-23 17:50:07
 //    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
 // ******************************************************************
+
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using RimWorld;
 using Verse;
 
 namespace SR.ModRimWorld.FactionalWar
 {
+    [UsedImplicitly]
     public class GenStepAirdropResource : GenStep
     {
         public override int SeedPart => 546950704;
+
         /// <summary>
         /// 生成
         /// </summary>
@@ -21,8 +27,19 @@ namespace SR.ModRimWorld.FactionalWar
         public override void Generate(Map map, GenStepParams parms)
         {
             //生成珍贵资源
+            var thingSetMakerParams = default(ThingSetMakerParams);
+            thingSetMakerParams.qualityGenerator = QualityGenerator.Reward;
+            thingSetMakerParams.makingFaction = Find.FactionManager.AllFactionsListForReading.RandomElement();
+            var totalThingList = new List<Thing>();
+            for (var i = 0; i < 5; i++)
+            {
+                var betrayalRewardThings =
+                    ThingSetMakerDefOf.MapGen_AncientTempleContents.root.Generate(thingSetMakerParams);
+                totalThingList.AddRange(betrayalRewardThings);
+            }
+
             //空投到中心
-            throw new System.NotImplementedException();
+            DropPodUtility.DropThingsNear(map.Center, map, totalThingList);
         }
     }
 }
--- Source/ModRimworldFactionalWar/WorldObject/Site/GenStepThreat.cs
+++ Source/ModRimworldFactionalWar/WorldObject/Site/GenStepThreat.cs
@@ -6,14 +6,21 @@
 //      /  \\        @Modified   2021-06-23 17:59:43
 //    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
 // ******************************************************************
+
+using System.Linq;
+using JetBrains.Annotations;
+using RimWorld;
 using Verse;
+using Verse.AI;
 
 namespace SR.ModRimWorld.FactionalWar
 {
+    [UsedImplicitly]
     public class GenStepThreat : GenStep
     {
         public override int SeedPart => 1254675168;
-        
+        private const int ThreatPoints = 5000;
+
         /// <summary>
         /// 生成
         /// </summary>
@@ -21,8 +28,23 @@ namespace SR.ModRimWorld.FactionalWar
         /// <param name="parms"></param>
         public override void Generate(Map map, GenStepParams parms)
         {
+            var incidentParms = new IncidentParms {points = ThreatPoints, faction = Faction.OfMechanoids, target = map};
+            var pawnGroupMakerParms =
+                IncidentParmsUtility.GetDefaultPawnGroupMakerParms(PawnGroupKindDefOf.Combat, incidentParms);
+            //生成失败 尝试用默认角色组生成器
+            var pawnList = PawnGroupMakerUtility.GeneratePawns(pawnGroupMakerParms).ToList();
+            //设置集群AI
+            foreach (var pawn in pawnList)
+            {
+                pawn.mindState.duty = new PawnDuty(RimWorld.DutyDefOf.Defend);
+            }
+
             //生成机械族或者虫族
-            throw new System.NotImplementedException();
+            foreach (var mechPawn in pawnList)
+            {
+                var loc = CellFinder.RandomClosewalkCellNear(map.Center, map, 5);
+                GenSpawn.Spawn(mechPawn, loc, map);
+            }
         }
     }
 }
